a11aad8e9002e2d373696daaa5cc71d6416e40e5
KnightKotOR <kirill2000kiril02@mail.ru>
2024-03-11 02:52:58 +0300

    "Маршрут по тайным тропам" init

---
+++ src/const/const.tsx
@@ -0,0 +1,12 @@
+export enum AppRoute {
+	Favorites = '/favorites',
+	Root = '/',
+	Login = '/login',
+	Offer = '/offer/:id'
+  }
+
+export enum AuthorizationStatus {
+	Auth = 'AUTH',
+	NoAuth = 'NO_AUTH',
+	Unknown = 'UNKNOWN',
+}
